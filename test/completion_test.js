891c79dfe417a31ff52c71a3496220678210ed4f
Todd Wolfson <todd@twolfson.com>
2013-12-15 14:12:20 -0800

    Outlining tests

--- test/completion_test.js
+++ test/completion_test.js
@@ -1,11 +1,25 @@
-var completion = require('../');
+var Completion = require('../');
 
-describe('completion', function () {
-  before(function () {
+describe('A partial command with one completion match', function () {
+  describe('being completed', function () {
+    it('returns its match', function () {
 
+    });
   });
+});
 
-  it('', function () {
+describe('A partial command with multiple completions', function () {
+  describe('being completed', function () {
+    it('returns all of its matches', function () {
 
+    });
+  });
+});
+
+describe('A partial command in junction with the item', function () {
+  describe('being completed', function () {
+    it('returns the command\'s match', function () {
+
+    });
   });
 });
