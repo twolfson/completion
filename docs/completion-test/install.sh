190482e8968556d3bc67ddd47a7151a85193df60
Todd Wolfson <todd@twolfson.com>
2013-12-16 01:03:54 -0800

    Adding test files for tab completion

---
+++ docs/completion-test/install.sh
@@ -0,0 +1,3 @@
+sudo mkdir -p /usr/local/etc/bash_completion.d/
+sudo ln -s $PWD/teeest /etc/bash_completion.d/teeest
+sudo ln -s $PWD/teeest.js /usr/local/bin/teeest
---
+++ docs/completion-test/teeest.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+var commands = ['test'];
+commands.forEach(function (cmd) {
+  console.log(cmd);
+});
